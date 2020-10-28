15b87f03446a1d7e1ff7c8b6d31e905f5ff5ec8e
Aiden Luo <aiden0xz@gmail.com>
2020-10-28 14:12:29 +0800

    Support change logo when change theme

--- static/src/theme.js
+++ static/src/theme.js
@@ -1,6 +1,7 @@
 window.onload = function () {
     var toggle = document.getElementById("dark-mode-toggle");
     var darkTheme = document.getElementById("dark-mode-theme");
+    var logo = document.getElementById("nav-logo");
 
     if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
         setTheme(localStorage.getItem("dark-mode-storage") || "dark");
@@ -21,9 +22,16 @@ window.onload = function () {
         if (mode === "dark") {
             darkTheme.disabled = false;
             toggle.className = "fas fa-sun";
+            if (logo.getAttribute("lightsrc")) {
+                logo.setAttribute("src", logo.getAttribute("lightsrc"));
+            }
+
         } else if (mode === "light") {
             darkTheme.disabled = true;
             toggle.className = "fas fa-moon";
+            if (logo.getAttribute("darksrc")) {
+                logo.setAttribute("src", logo.getAttribute("darksrc"));
+            }
         }
     }
 }
